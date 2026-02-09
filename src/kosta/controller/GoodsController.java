f769a2e50001c3941d6f460d1450cd55395ab82c
hyuk-dev <hyuk.development@gmail.com>
2026-02-09 17:26:40 +0900

    집에서 작업함

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -13,5 +13,9 @@ public class GoodsController {
 		System.out.println("원격에서 test추가함 - 시나리오 5번 테스트");
 		System.out.println("로컬에서 수정을 하고 있어요.");
 	}
+	
+	public void selectAll() {
+		System.out.println("아 잠온다");
+	}
 
 }
