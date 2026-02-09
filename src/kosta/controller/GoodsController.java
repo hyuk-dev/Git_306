14be659b06ec5d86a662fa0224c5487a3b14da16
이동혁 <hyuk.development@gmail.com>
2026-02-09 16:06:11 +0900

    Remove test method from GoodsController
    Removed the test method from the main method.

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -5,9 +5,6 @@ public class GoodsController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 10;
-		public void test() {
-			System.out.println(i);
-		}
 	}
 
 }
