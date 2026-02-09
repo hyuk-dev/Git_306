6002140ab0fbeb01106a29619b23aa1e2558f1dd
hyuk-dev <hyuk.development@gmail.com>
2026-02-09 16:04:52 +0900

    시나리오 3번 - GoodsService 수정

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -1,13 +1,14 @@
 package kosta.controller;
 
 public class GoodsController {
-
+	int i = 10;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 10;
-		public void test() {
-			System.out.println(i);
-		}
+		
+	}
+	public void test() {
+		System.out.println(i);
 	}
 
 }
--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -5,6 +5,11 @@ public class GoodsService {
 	public void insert() {
 		System.out.println("insert call...");
 	}
+	
+	public void select() {
+		System.out.println("select 추가...");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
