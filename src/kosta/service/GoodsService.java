1cddd9b4796e000d10f9e1a834fe6adb0bee8899
hyuk-dev <hyuk.development@gmail.com>
2026-02-09 14:54:06 +0900

    third commit-service

---
+++ src/kosta/service/GoodsService.java
@@ -0,0 +1,13 @@
+package kosta.service;
+
+public class GoodsService {
+
+	public void insert() {
+		System.out.println("insert call...");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
