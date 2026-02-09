812c865f377d579a72d1214daa02fa66d23e02da
hyuk-dev <hyuk.development@gmail.com>
2026-02-09 17:03:32 +0900

    conflict 해결

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -10,7 +10,11 @@ public class GoodsController {
 		System.out.println(i);
 		System.out.println(i);
 		System.out.println(i);
+<<<<<<< Updated upstream
 		System.out.println("원격에서 test추가함 - 시나리오 5번 테스트");
+=======
+		System.out.println("로컬에서 수정을 하고 있어요.");
+>>>>>>> Stashed changes
 	}
 
 }
