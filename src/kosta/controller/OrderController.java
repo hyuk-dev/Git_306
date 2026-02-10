a55c09f99c235ef2fe6c795d0df5a48caaded182
이동혁 <hyuk.development@gmail.com>
2026-02-10 12:18:00 +0900

    Add remoteCart method to OrderController

--- src/kosta/controller/OrderController.java
+++ src/kosta/controller/OrderController.java
@@ -4,4 +4,8 @@ public class OrderController {
 	public void orderInsert() {
 		System.out.println("order 브랜치에서 추가");
 	}
+
+	public void remoteCart() {
+		System.out.println("원격 main브랜치에서 추가");
+	}
 }
